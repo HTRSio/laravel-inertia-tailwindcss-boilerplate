e41f7cd66ca58155fa55aae19319c3c259f324f0
gereongoldbach <info@htrs.io>
2022-05-20 18:26:30 +0200

    Rename and reformat the files for multi-word-component

---
+++ resources/js/Pages/TheHome.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Head>
+      <title>Home</title>
+      <meta
+        type='description'
+        content='Home information'
+        head-key='description'
+      >
+    </Head>
+    <h1 class='text-3xl'>
+      Home
+    </h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'homePage',
+};
+</script>
