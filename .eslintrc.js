54036c540fc7a7bc5e219f4962d6e4b20ee8fc68
gereongoldbach <info@htrs.io>
2022-05-20 18:24:53 +0200

    Config eslintrc.js for vue3-essential

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,22 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: ["eslint:recommended", "plugin:vue/vue3-recommended"],
-    parserOptions: {
-        ecmaVersion: 12,
-        sourceType: "module",
-    },
-    plugins: ["vue"],
-    rules: {
-        indent: ["error", 2],
-        "linebreak-style": ["error", "unix"],
-        quotes: ["error", "double"],
-        semi: ["error", "always"],
+  env: {
+    browser: true, es2021: true, node: true,
+  }, extends: ['eslint:recommended', 'plugin:vue/vue3-essential'], parserOptions: {
+    ecmaVersion: 12, sourceType: 'module',
+  }, plugins: ['vue'], rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
 
-        "vue/order-in-components": ["error"],
-        "vue/require-default-prop": ["off"],
-        "vue/max-attributes-per-line": [
-            "error",
-            {
-                singleline: {
-                    max: 2,
-                    allowFirstLine: true,
-                },
-            },
-        ],
-    },
-    globals: { require: true, defineProps: true },
+    'vue/order-in-components': ['error'],
+    'vue/require-default-prop': ['off'],
+    'vue/max-attributes-per-line': ['error', {
+      singleline: {
+        max: 2,
+      }, multiline: {
+        max: 2,
+      },
+    }],
+  }, globals: { require: true, defineProps: true },
 };
